1f23cdf10dc7c6b9948de41840e6f328b3f844db
Surogate <errata.dark@gmail.com>
2017-09-11 20:15:41 +0200

    fixes Appveyor & some basic_list stuff

--- test/test_list.cpp
+++ test/test_list.cpp
@@ -7,12 +7,13 @@ void test_list()
 	{
 		fixed::list<int, 20> l;
 		fixed::list<int, 30, fixed::_impl::basic_heap_allocator> list_on_heap;
+		fixed::_impl::empty_source alloc_source_inst;		
 
-		fixed::list<int, 20> l_with_alloc_source(fixed::_impl::empty_source());
-		fixed::list<int, 20> l_with_values(5, 2, fixed::_impl::empty_source());
-		fixed::list<int, 20> l_with_size(5, fixed::_impl::empty_source());
+		fixed::list<int, 20> l_with_alloc_source(alloc_source_inst);
+		fixed::list<int, 20> l_with_values(5, 2, alloc_source_inst);
+		fixed::list<int, 20> l_with_size(5, alloc_source_inst);
 		auto values = { 1, 2, 3, 4, 5 };
-		fixed::list<int, 20> l_with_iterator(values.begin(), values.end(), fixed::_impl::empty_source());
+		fixed::list<int, 20> l_with_iterator(values.begin(), values.end(), alloc_source_inst);
 	}
 
 	{
