74a634d6017b2ef995b11d6cb3d4710dc4010ea2
Francois A <francoisancel@gmail.com>
2017-10-13 13:40:41 +0200

    format of fixed_type_traits.hpp

--- include/fixed/impl/fixed_type_traits.hpp
+++ include/fixed/impl/fixed_type_traits.hpp
@@ -11,43 +11,43 @@ namespace astd
 
     template <class T> constexpr bool is_const_v = std::is_const<T>::value;
 
-	template <bool val> struct constexpr_if
-	{
-		template <typename CLOSURE_T> constexpr constexpr_if(const CLOSURE_T&)
-		{
-		}
+    template <bool val> struct constexpr_if
+    {
+        template <typename CLOSURE_T> constexpr constexpr_if(const CLOSURE_T&)
+        {
+        }
 
-		template <typename CLOSURE_T> constexpr void _else(const CLOSURE_T& f)
-		{
-			f();
-		}
-	};
+        template <typename CLOSURE_T> constexpr void _else(const CLOSURE_T& f)
+        {
+            f();
+        }
+    };
 
-	template <> struct constexpr_if<true>
-	{
-		template <typename CLOSURE_T> constexpr constexpr_if(const CLOSURE_T& f)
-		{
-			f();
-		}
+    template <> struct constexpr_if<true>
+    {
+        template <typename CLOSURE_T> constexpr constexpr_if(const CLOSURE_T& f)
+        {
+            f();
+        }
 
-		template <typename CLOSURE_T> constexpr void _else(const CLOSURE_T&) {}
-	};
+        template <typename CLOSURE_T> constexpr void _else(const CLOSURE_T&) {}
+    };
 
-	template <typename T, class = void> struct is_iterator : public std::false_type
-	{
-	};
+    template <typename T, class = void>
+    struct is_iterator : public std::false_type
+    {
+    };
 
-	template <typename T>
-	struct is_iterator<T,
-		fixed::astd::void_t<typename std::iterator_traits<T>::iterator_category>>
-		: public std::true_type
-	{
-	};
+    template <typename T>
+    struct is_iterator<T,
+        fixed::astd::void_t<
+            typename std::iterator_traits<T>::iterator_category>>
+        : public std::true_type
+    {
+    };
 
-	template <typename T>
-	constexpr bool is_iterator_v = is_iterator<T>::value;
+    template <typename T> constexpr bool is_iterator_v = is_iterator<T>::value;
 }
-
 }
 
 #endif //! FIXED_VOID_T_COMPATIBILITY_HPP
